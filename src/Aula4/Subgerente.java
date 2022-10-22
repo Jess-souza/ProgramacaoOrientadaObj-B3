a7d2f65a3a5dd18cf49766d5b26b1c00c7f60d39
Jessica <jsouzasouza120@gmail.com>
2022-10-21 21:26:20 -0300

    Inclusão da aula 4 - Herança

---
+++ src/Aula4/Subgerente.java
@@ -0,0 +1,9 @@
+package Aula4;
+
+import java.math.BigDecimal;
+
+public class Subgerente extends Gerente {
+    public Subgerente(String nome, int matricula, BigDecimal salario) {
+        super(nome, matricula, salario);
+    }
+}
